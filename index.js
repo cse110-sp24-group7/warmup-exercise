dede8e8ec6d36b0482ef05792019051ca38a7c9e
SB1019 <sasrinath@ucsd.edu>
2024-04-25 17:47:30 -0700

    Ran Prettier

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const form = document.getElementById('sentiment-form');
+const form = document.getElementById("sentiment-form");
 
 form.onsubmit = (e) => {
-    let rank = document.querySelector('input[name="sentiment"]:checked').value;
-    let notes = document.getElementById('sentiment-notes').value;
+  let rank = document.querySelector('input[name="sentiment"]:checked').value;
+  let notes = document.getElementById("sentiment-notes").value;
 
-    localStorage.setItem("sentiment-rank", rank);
-    localStorage.setItem("sentiment-notes", notes);
-}
+  localStorage.setItem("sentiment-rank", rank);
+  localStorage.setItem("sentiment-notes", notes);
+};
